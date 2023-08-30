6c122bce1ed589a4b394b2aa52ca5f21ad28aac2
sepehrr <8377854+sepehrr@users.noreply.github.com>
2023-08-30 14:53:55 +0330

    ✅ Pass futureevents test

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -182,6 +182,8 @@ class EventsController extends BaseController
 
     public function getFutureEventsWithWorkshops()
     {
-        throw new \Exception('implement in coding task 2');
+        return Event::with('workshops')->whereHas('workshops', function ($query) {
+            $query->where('start', '>', Date::now());
+        })->get();
     }
 }
